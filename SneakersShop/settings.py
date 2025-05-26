f4a427ac093eb26cfac33ba4b33df1b2d4048fb5
Danil <danil.piat@mail.ru>
2025-05-26 20:11:26 +0300

    settings change

--- SneakersShop/settings.py
+++ SneakersShop/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/product_images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
