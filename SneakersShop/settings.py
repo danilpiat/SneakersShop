1d1d1b7f530412e7d17dea6a23678a14aae49d86
Danil <danil.piat@mail.ru>
2025-05-26 20:13:17 +0300

    settings change

--- SneakersShop/settings.py
+++ SneakersShop/settings.py
@@ -125,11 +125,19 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+# Путь для сбора статических файлов при deploy
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/product_images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
+# Дополнительные директории статических файлов
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+# Медиа файлы (загрузки пользователей)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
